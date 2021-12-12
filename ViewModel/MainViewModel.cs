0629a937b984cec25a58cdd13310fa015eafcedb
tranleconghau77 <tranleconghau772001@gmail.com>
2021-12-12 21:25:56 +0700

    Project Manager drink C#

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace manager_drink.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded=false;
+        public ICommand LoadedWindowCommand { get; set; }
+
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+            Isloaded=true;
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+            });
+
+        }
+    }
+}
